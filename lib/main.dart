976c6009663692420c1ace918067dca3e0dc3e50
mpGit90 <bhattganga216@gmail.com>
2022-12-26 06:53:42 +0545

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogue/home_page.dart';
-import 'home_page.dart';
+import 'package:flutter_catalogue/pages/home_page.dart';
+import 'package:flutter_catalogue/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.lime),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          "LoginPage",
+          style: TextStyle(
+              fontSize: 30, color: Colors.yellow, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
